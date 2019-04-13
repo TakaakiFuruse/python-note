6ce1e63693bf13294c43be00bd5c20d1df17d03e
TakaakiFuruse <takaakifuruse@gmail.com>
2019-04-13 16:08:26 +0900

    cli.py created

--- clean_arch/rent_o_matic/rent_o_matic/cli.py
+++ clean_arch/rent_o_matic/rent_o_matic/cli.py
@@ -1,15 +1,32 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-import click
+from rent_o_matic.repository import memrepo as mr
+from rent_o_matic.use_cases import room_list_use_case as uc
 
+room1 = {
+    "code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+    "size": 215,
+    "price": 39,
+    "longitude": -0.09998975,
+    "latitude": 51.75436293,
+}
+room2 = {
+    "code": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+    "size": 405,
+    "price": 66,
+    "longitude": 0.18228006,
+    "latitude": 51.74640997,
+}
+room3 = {
+    "code": "913694c6-435a-4366-ba0d-da5334a611b2",
+    "size": 56,
+    "price": 60,
+    "longitude": 0.27891577,
+    "latitude": 51.45994069,
+}
 
-@click.command()
-def main(args=None):
-    """Console script for rent_o_matic"""
-    click.echo("Replace this message by putting your code into "
-               "rent_o_matic.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+repo = mr.MemRepo([room1, room2, room3])
+use_case = uc.RoomListUseCase(repo)
+result = use_case.execute()
 
-
-if __name__ == "__main__":
-    main()
+print([room.to_dict() for room in result])
